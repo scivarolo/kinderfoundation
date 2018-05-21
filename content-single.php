814435f437f31ceecdaa44b0e8eeb4c1f42b2c03
Sebastian Civarolo <sebastian@coredesignstudio.com>
2018-05-21 12:22:54 -0500

    Stops posts opening in new window and removes "blog post" from top

--- content-single.php
+++ content-single.php
@@ -6,19 +6,19 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('post'); ?>>
 	<header class="entry-header">
-		<?php echo get_the_category_list(); ?>
-		
+		<?php /* echo get_the_category_list(); */ ?>
+
 		<div class="entry-meta">
 		  <span class="entry-date"><?php the_time( get_option('date_format') ); ?></span>
 		  <?php if( get_field('source_name') ) : ?> <span class="entry-source">&mdash; <?php the_field('source_name'); ?> </span><?php endif; ?>
 		</div>
-		
-    <h1 class="entry-title"><a href="<?php the_field('source_url'); ?>" target="_blank"><?php the_title(); ?></a></h1>
-    
+
+    <h1 class="entry-title"><a href="<?php the_field('source_url'); ?>"><?php the_title(); ?></a></h1>
+
     <div class="entry-organization">
       <?php echo get_the_term_list( $post->ID, 'organization', '<span class="organization-label">Organization:</span> ', ', ' ); ?>
     </div>
-		
+
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
--- content.php
+++ content.php
@@ -7,25 +7,26 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('post'); ?>>
 	<header class="entry-header">
 		<?php /* echo get_the_category_list(); */ ?>
-		
-		<div class="entry-meta">
-		  <span class="entry-date"><?php the_time( get_option('date_format') ); ?></span>
+
+    <h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+
+    <div class="entry-meta">
+
 		  <?php if( get_field('source_name') ) : ?> <span class="entry-source">&mdash; <?php the_field('source_name'); ?> </span><?php endif; ?>
+      <span class="entry-date"><?php the_time( get_option('date_format') ); ?></span>
 		</div>
-		
-    <h1 class="entry-title"><a href="<?php the_permalink(); ?>" target="_blank"><?php the_title(); ?></a></h1>
-    
+
     <div class="entry-organization">
       <?php echo get_the_term_list( $post->ID, 'organization', '<span class="organization-label">Organization:</span> ', ', ' ); ?>
     </div>
-		
+
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php
 			/* translators: %s: Name of current post */
 			the_content( sprintf(
-				__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'kinderfoundation' ), 
+				__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'kinderfoundation' ),
 				the_title( '<span class="screen-reader-text">"', '"</span>', false )
 			) );
 		?>
