fc78fac6d0f9ae11daa74425711c824dcfa07408
Sebastian Civarolo <sebastian@coredesignstudio.com>
2018-05-21 16:48:32 -0500

    Refines post headers and increases text size at 1200+

--- content-single.php
+++ content-single.php
@@ -6,18 +6,26 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('post'); ?>>
 	<header class="entry-header">
-		<?php /* echo get_the_category_list(); */ ?>
 
-		<div class="entry-meta">
-		  <span class="entry-date"><?php the_time( get_option('date_format') ); ?></span>
-		  <?php if( get_field('source_name') ) : ?> <span class="entry-source">&mdash; <?php the_field('source_name'); ?> </span><?php endif; ?>
-		</div>
+    <div class="entry-date"><?php the_time( 'M j, Y' ); ?></div>
 
-    <h1 class="entry-title"><a href="<?php the_field('source_url'); ?>"><?php the_title(); ?></a></h1>
+    <h1 class="entry-title">
+      <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+    </h1>
 
-    <div class="entry-organization">
-      <?php echo get_the_term_list( $post->ID, 'organization', '<span class="organization-label">Organization:</span> ', ', ' ); ?>
+    <?php $terms = get_the_term_list( $post->ID, 'organization', '<span class="organization-label">Organization:</span> ', '<span class="org-comma">, </span>' );
+    if($terms) : ?>
+
+    <div class="entry-meta">
+
+      <div class="entry-organization">
+        <?php echo $terms; ?>
+      </div>
+
+      <?php if( get_field('source_name') ) : ?> <span class="entry-source">&mdash; <?php the_field('source_name'); ?> </span><?php endif; ?>
     </div>
+
+    <?php endif; ?>
 
 	</header><!-- .entry-header -->
 
--- content.php
+++ content.php
@@ -6,23 +6,31 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('post'); ?>>
 	<header class="entry-header">
-		<?php /* echo get_the_category_list(); */ ?>
 
-    <h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+    <div class="entry-date"><?php the_time( 'M j, Y' ); ?></div>
+
+    <h1 class="entry-title">
+      <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+    </h1>
+
+    <?php $terms = get_the_term_list( $post->ID, 'organization', '<span class="organization-label">Organization:</span> ', '<span class="org-comma">, </span>' );
+    if($terms) : ?>
 
     <div class="entry-meta">
 
-		  <?php if( get_field('source_name') ) : ?> <span class="entry-source">&mdash; <?php the_field('source_name'); ?> </span><?php endif; ?>
-      <span class="entry-date"><?php the_time( get_option('date_format') ); ?></span>
-		</div>
+      <div class="entry-organization">
+        <?php echo $terms; ?>
+      </div>
 
-    <div class="entry-organization">
-      <?php echo get_the_term_list( $post->ID, 'organization', '<span class="organization-label">Organization:</span> ', ', ' ); ?>
+      <?php if( get_field('source_name') ) : ?> <span class="entry-source">&mdash; <?php the_field('source_name'); ?> </span><?php endif; ?>
     </div>
+
+    <?php endif; ?>
 
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
+
 		<?php
 			/* translators: %s: Name of current post */
 			the_content( sprintf(
